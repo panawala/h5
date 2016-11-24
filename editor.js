64815c40bd25fec95adef6fb7b7997beac72a5fe
panawala <william@xmonster.cn>
2016-11-24 22:29:22 +0800

    增加一些备用

---
+++ editor.js
@@ -0,0 +1,95 @@
+/**
+ * Created by william on 2016/11/24.
+ */
+
+
+//段落标题  
+var paraph_title_1 = {
+    type: 1,
+    class: 'go-title-dianpu'
+}
+
+var paraph_title_2 = {
+    type: 1,
+    class: 'go-title-zhuyishixiang'
+}
+
+var paraph_title_3 = {
+    type: 1,
+    class: 'go-title-zouqidaren'
+}
+
+//大标题
+var h1_test_1 = {
+    type: 2,
+    class: 'go-h1-underline',
+    content: '当中式青花瓷碰上欧式新古典'
+}
+
+var h1_test_2 = {
+    type: 2,
+    class: 'go-h1-around',
+    content: '玛丽莲梦露与毛主席"同台"'
+}
+
+//小标题
+var h2_test_1 = {
+    type: 3,
+    class: 'go-h2-counter-triangle',
+    content: '翻糖蛋糕-3月优雅上市'
+}
+
+var h2_test_2 = {
+    type: 3,
+    class: 'go-h2-number',
+    content: '1 | Elephant Nature Park'
+}
+
+//正文
+var body_test = {
+    type: 4,
+    class: 'go-body',
+    content: '春暖花开的季节,GAWT即将上线新的翻糖蛋糕和甜品台高端定制服务,翻糖蛋糕是源自于英国的艺术蛋糕,' +
+    '延展性极佳的翻糖可以塑造出各种各样的造型,并将精细特色完美的展现出来延展性极佳的翻糖可以塑造出各种各样的造型,' +
+    '并将精细特色完美的展现出来延展性极佳的翻糖可以塑造出各种各样的造型,并将精细特色完美的展现出来延展性极佳的翻糖' +
+    '可以塑造出各种各样的造型,并将精细特色完美的展现出来延展性极佳的翻糖可以塑造出各种各样的造型,并将精细特色完美的展现出来'
+}
+
+// 图片
+var img_test = {
+    type: 5,
+    class: 'go-img',
+    content: 'http://7tszlo.com1.z0.glb.clouddn.com/7bc98628-b0b4-11e6-b276-00163e002e64.png',
+    footnote: 'Andy Warhol的玛丽莲梦露'
+}
+
+// 正文列表
+var li_test_1 = {
+    type: 6,
+    class: 'go-li-number',
+    content: ['11月初最适合情侣的标准好莱坞爆米花电影,也是同档期内约会电影的的首选',
+        '"神奇女侠"盖尔加多与"广告狂人家哈姆惊喜上线新版史密斯夫妇,碰撞猛烈火花',
+        '导演说曾经执导过大热美剧《新闻编辑室》的格雷格莫拓拉']
+}
+
+var li_test_2 = {
+    type: 6,
+    class: 'go-li-triangle',
+    content: ['老师介绍盆摘/吊篮/永生花制作的基本信息',
+        '依据所选套餐分发DIY材料',
+        '讲解具体制作方法',
+        '挑选植株亲手操作',
+        '动手制作自己的绿色作品',
+        '养护知识教学',
+        '共计: 120min']
+}
+
+// 走起达人div
+var go_expert = {
+    type: 7,
+    class: 'go-expert',
+    content: '当咖啡遇到多肉的创始人。如今的他致力于把对多肉感兴趣的人集合到一起,希望能把多肉养殖的乐趣传递给更多人',
+    user_id: 2389,
+    avatar: 'http://7tszlo.com1.z0.glb.clouddn.com/7e063972-b0b4-11e6-b276-00163e002e64.png',
+    name: '夏夜'
+}
